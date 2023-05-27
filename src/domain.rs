ff207741a74a486fd3ba261378df7372c97f3c7b
chris9740 <33673505+chris9740@users.noreply.github.com>
2023-05-27 16:36:21 +0200

    refactor: revise parse function for domains

--- src/domain.rs
+++ src/domain.rs
@@ -1,9 +1,20 @@
-// DNS requests include domains that end with a dot, such as 'google.com.',
-// and this function removes any unnecessary dots
-fn parse(name: &str) -> String {
-    let parsed: Vec<&str> = name.split(".").filter(|label| label.len() > 0).collect();
+// Fully qualified domain names (FQDN) end with a seemingly redundant dot,
+// representing an empty label (this is to make it unambiguous).
+//
+// Domains in our filter lists are _partially_ qualified, meaning they don't include
+// the empty label at the end, so we will strip it out.
+//
+// https://en.wikipedia.org/wiki/Fully_qualified_domain_name
+fn parse(name: &str) -> &str {
+    if !name.ends_with(".") {
+        return name;
+    }
 
-    parsed.join(".")
+    let mut chars = name.chars();
+
+    chars.next_back();
+
+    chars.as_str()
 }
 
 #[derive(Clone)]
@@ -15,7 +26,7 @@ impl From<&str> for Domain {
     fn from(value: &str) -> Self {
         let name = parse(value);
 
-        Domain { name }
+        Domain { name: name.to_string() }
     }
 }
 
@@ -26,5 +37,6 @@ mod tests {
     #[test]
     fn parses_domain() {
         assert_eq!(Domain::from("signal.org.").name, "signal.org");
+        assert_eq!(Domain::from("signal.org").name, "signal.org");
     }
 }
