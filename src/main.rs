243fb77c58da723afae553148b7c716f04b590ce
chris9740 <33673505+chris9740@users.noreply.github.com>
2025-05-31 05:46:44 +0200

    test: add tracing and gate it behind feature

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use anyhow::Result;
 use std::{io::ErrorKind, process};
 use swiftdns::cli;
+#[cfg(feature = "tracing")]
+use tracing::level_filters::LevelFilter;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
+    #[cfg(feature = "tracing")]
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::DEBUG)
+        .init();
+
     if let Err(err) = cli::start().await {
         if let Some(io_err) = err.downcast_ref::<std::io::Error>() {
             if io_err.kind() == ErrorKind::BrokenPipe {
